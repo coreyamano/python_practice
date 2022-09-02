ad5894612d256769669f16d81a588b8e205ecda5
Corey <corey.amano@gmail.com>
2022-09-01 21:49:33 -0700

    set up string algo ladder problems

---
+++ basic_string_algo_ladder.py
@@ -0,0 +1,50 @@
+# 1. Reverse String
+# Write a function that returns the reverse of a given string.
+# Input: “abcde”
+# Output: “edcba”
+
+
+# 2. Show Me The Money
+# Description
+# Given a string, write a function that returns true if the “$” character is contained within the string or false if it is not.
+# Input: “i hate $ but i love money i know i know im crazy”
+# Output: true
+# Input: “abcdefghijklmnopqrstuvwxyz”
+# Output: false
+
+# 3. Alternate Capitals
+
+# Description
+# Given a string, write a function that returns a copy of the original string that has every other character capitalized. (Capitalization should begin with the second character.)
+# Input: “hello, how are your porcupines today?”
+# Output: “hElLo, HoW ArE YoUr pOrCuPiNeS ToDaY?”
+
+# 4. First Duplicate CharacterData
+# Description
+# Given a string, write a function that returns the first occurence of two duplicate characters in a row, and return the duplicated character.
+# Input: “abcdefghhijkkloooop”
+# Output: “h”
+
+# 5. Palindrome
+# Description
+# Given a string, write a function that returns true if it is a palindrome, and false if it is not. (A palindrome is a word that reads the same both forward and backward.)
+# Input: “racecar”
+# Output: true
+# Input: “baloney”
+# Output: false
+
+# 6. Hamming ->
+# Description
+# Given two strings of equal length, write a function that returns the number of characters that are different between the two strings.
+# Input: "ABCDEFG", "ABCXEOG"
+# Output: 2
+# Explanation: While the A, B, C, E, and G are in the same place for both strings, they have different characters in the other spaces. Since there are 2 such spaces that are different(the D and F in the first string), we return 2.
+# Input: "ABCDEFG", "ABCDEFG",
+# Output: 0
+
+# 7. Reverse Words
+
+# Description
+# Given a string of words, write a function that returns a new string that contains the words in reverse order.
+# Input: “popcorn is so cool isn’t it yeah i thought so”
+# Output: “so thought i yeah it isn’t cool so is popcorn”
