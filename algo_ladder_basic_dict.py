339ec9d9bd2b3b4cf77b15fd26e59e9c57f88898
coreyamano <corey.amano@gmail.com>
2022-09-18 20:22:04 -0700

    complete problem 6 in algo basic dict

--- algo_ladder_basic_dict.py
+++ algo_ladder_basic_dict.py
@@ -151,6 +151,24 @@ print(give_rna('ACGTGGTCTTAA'))
 # {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
 # ]
 
+def list_by_id(posts, users):
+    for p in posts:
+        n = users.get(p['submitted_by'])
+        p['submitted_by'] = n
+    return posts
+
+
+print(list_by_id(
+    [
+    {'title': 'Me Eating Pizza', 'submitted_by': 231, 'likes': 1549},
+    {'title': 'i never knew how cool i was until now', 'submitted_by': 989, 'likes': 3},
+    {'title': 'best selfie evar!!!', 'submitted_by': 111, 'likes': 1092},
+    {'title': 'Mondays are the worst', 'submitted_by': 403, 'likes': 644}
+    ],
+    {403:"Aunty Em", 231:"Joelle P.", 989:"Lyndon Johnson", 111:"Patti Q."}
+))
+
+
 # 7. Anagrams
 
 # Description
