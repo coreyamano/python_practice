35fdcbb69a89979434939d5da1d54fd51b5a92e8
coreyamano <corey.amano@gmail.com>
2022-10-18 09:09:14 -0700

    fix 2nd half of happy number

--- leetcode_happy_number.py
+++ leetcode_happy_number.py
@@ -2,15 +2,18 @@ def happy_number(num):
     result = num
     sum = 0
 
-    while result != 1:
+    while result != 1 and len(str(result)) != 1:
         for n in str(result):
             sum += int(n)**2
         result = sum
         sum = 0
+
     if result == 1:
         return True
+    else:
+        return False
 
     
 print(happy_number(19))
-
+print(happy_number(2))
 
